52223600f22651560a9d20f40332e045d86881c1
horwellmv <horwellmv@gmail.com>
2020-11-23 22:01:29 -0300

    Avance logrado, Desarrollado punto a y b, En desarrrolo c y falta d

--- recuperatorio2.py
+++ recuperatorio2.py
@@ -2,14 +2,116 @@ import csv
 
 
 
-def decodificar():
-    print("decodificamos")
-    return
+def decodificar(archivoCodigo):
+
+    print("\tDECODIFICANDO ARCHIVO .CSV\n")
 
 
-def analizar():
-    print("analizamos")
-    return
+    try:
+        with open(archivoCodigo) as fCodificado:
+            codificadoCsv = csv.reader(fCodificado, delimiter = ",")
+
+            for i in codificadoCsv:
+
+                mapeo = {
+                    ord ('?'): None,
+                    ord ('%'): None,
+                    ord ('2'): None,
+                    ord ('0'): " "
+                }
+                index0 = i[0].translate(mapeo)
+                index1 = i[1].translate(mapeo)
+                index2 = i[2]
+
+                print (f"\t{index0}, {index1}, {index2}")
+
+        return
+
+    except FileNotFoundError:
+        print("No se pudo abrir el archivo, o no existe.")
+        return
+
+
+
+
+
+
+
+
+def analizar(archivoCodigo):
+
+
+
+    print("\tANALIZANDO ARCHIVO .CSV\n")
+
+
+    try:
+        listaDicionarios =[]
+        cabeceras = ["beneficiario", "prestacion", "monto"]
+
+        with open(archivoCodigo) as fCodificado:
+            codificadoCsv = csv.reader(fCodificado, delimiter = ",")
+
+            for i in codificadoCsv:
+                diccionario = {}
+                mapeo = {
+                    ord ('?'): None,
+                    ord ('%'): None,
+                    ord ('2'): None,
+                    ord ('0'): " "
+                }
+                index0 = i[0].translate(mapeo)
+                index1 = i[1].translate(mapeo)
+                index2 = i[2]
+
+                diccionario["beneficiario"] = index0
+                diccionario["prestacion"] = index1
+                diccionario["monto"] = index2
+
+                listaDicionarios.append(diccionario)
+
+
+        with open("analisis.csv", "w") as f:
+            nuevoAnalisis = csv.DictWriter(f,fieldnames=cabeceras)
+
+            nuevoAnalisis.writeheader()
+            nuevoAnalisis.writerows(listaDicionarios)
+
+
+        print(f"\t.csv ha sido creado con exito. ")
+        print(listaDicionarios)
+
+
+
+    except FileNotFoundError:
+        print("No se pudo abrir el archivo, o no existe.")
+        return
+
+
+def nuevoArchivo(archivoAnalisis):
+    cabecera2 = ["beneficiario","total","auditoria"]
+    with open("verificado.csv", "w") as nuevoF, open(archivoAnalisis) as origen:
+        nuevoCsv = csv.DictWriter(nuevoF, fieldnames= cabecera2)
+        origenCsv = csv.DictReader(origen)
+
+        lineaO = next(origenCsv, None)
+        lineaN = next(nuevoCsv, None)
+
+        monto = 0 
+        while lineaO:
+
+                        while alumno:
+                            print(f"{alumno[0]}")
+                            if not nota or nota[0] != alumno[0]:
+                                print("\tNo se registran notas")
+
+                            while nota and nota[0] == alumno[0]:
+                                print(f"\t{alumno[0]} en {nota[1]}: {nota[2]}")
+                                nota = next(notas_csv, None)
+                            alumno = next(alumnos_csv, None)
+
+
+
 
 
 def imprimir():
@@ -29,10 +131,11 @@ def principal():
             menu = int(input("\t\t : "))
 
             if menu == 1:
-                decodificar()
+                decodificar("prestaciones.csv")
+
 
             if menu == 2:
-                analizar()
+                analizar("prestaciones.csv")
 
             if menu == 3:
                 imprimir()
@@ -46,3 +149,6 @@ def principal():
             return principal()
 
 principal()
+
+
+# print(x[2])
